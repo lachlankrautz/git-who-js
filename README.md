6c59332950971136b3b3b2ad2636f0443d85e6ff
Lachlan Krautz <lachlan.krautz@gmail.com>
2016-07-25 19:48:00 +1000

    working

---
+++ README.md
@@ -0,0 +1,19 @@
+git-who
+=================
+
+List remote branches by author and date of last commit
+ - Coloured dates highlight age
+
+# Example
+
+```shell
+$ git-who
+[2016-06-10] James Kirk    shore-leave-locations
+[2016-06-17] James Kirk    harry-mudd-emails
+[2016-05-24] Leonard McCoy reasons-spock-not-fit-for-command
+[2016-06-21] Leonard McCoy dead-officers-list
+[2016-06-21] Leonard McCoy doctor-not-a
+[2015-03-11] Spock         talos-iv-return-plan
+[2015-09-16] Spock         3d-chess-notes
+[2015-10-12] Spock         hands-on-reactor-repair-guide
+```
--- index.js
+++ index.js
@@ -5,24 +5,66 @@
 //program.version('0.1');
 //program.parse(process.argv);
 
-const spawn = require('child_process').spawn;
-const git = spawn('git', ['for-each-ref', '--format=%(authorname)^~^%(refname:short)^~^%(committerdate:rfc2822)']);
+const spawn  = require('child_process').spawn;
+const git    = spawn('git', [
+    'for-each-ref',
+    '--format=%(authorname)^~^%(refname:short)^~^%(committerdate:iso8601)',
+    'refs/remotes/origin'
+]);
+const moment = require('moment');
+const colors = require('colors');
+
+var day_90  = moment();
+var day_180 = moment();
+day_90.subtract(90, "day");
+day_180.subtract(180, "day");
+
+function get_coloured_date (date) {
+    var str = date.format("YYYY-MM-DD");
+    if (date < day_180) {
+	str = colors.red(str);
+    }
+    else if (date < day_90) {
+	str = colors.yellow(str);
+    } else {
+	str = colors.green(str);
+    }
+    return str;
+}
 
 git.stdout.on('data', (data) => {
 
-    // console.log(`stdout: ${data}`);
-    var lines = data.toString().split(/\r?\n/);
+    var lines    = data.toString().split(/\r?\n/);
+    var branches = [];
+    var max      = 0;
     lines.forEach((line) => {
-	// console.log(`${line}\n`);
-	var parts = line.split("~-~");
-	parts.forEach((part) => {
-	    console.log(`part: ${part}`);
+	if (!line || line.includes("origin/HEAD") || line.includes("origin/master")) {
+	    return;
+	}
+	var parts = line.replace("origin/", "").split("^~^");
+	branches.push({
+	    date:   moment(parts[2]),
+	    author: parts[0],
+	    name:   parts[1]
 	});
+	max = Math.max(max, parts[0].length);
     });
-    
+    branches.sort((a, b) => {
+	var order = a.author.localeCompare(b.author);
+	if (order == 0) {
+	    order = a.date > b.date;
+	}
+	return order;
+    });
+    branches.forEach((branch) => {
+	var gap   = " ".repeat(max - branch.author.length);
+	var date  = branch.date;
+	var clean = `[${get_coloured_date(date)}] ${branch.author}${gap} ${branch.name}`;
+	console.log(`${clean}`);
+    });
 });
 
 git.stderr.on('data', (data) => {
-    console.log(`Not a git repo`);
+    console.log(`Unable to display branches`);
     process.exit(0);
 });
