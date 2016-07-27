7662d4082287788e69f9bb596675a96e8c01fd5c
Lachlan Krautz <lachlan.krautz@gmail.com>
2016-07-27 22:17:29 +1000

    use functional style

--- index.js
+++ index.js
@@ -22,49 +22,54 @@ day_180.subtract(180, "day");
 function get_coloured_date (date) {
     var str = date.format("YYYY-MM-DD");
     if (date < day_180) {
-	str = colors.red(str);
+	    str = colors.red(str);
     }
     else if (date < day_90) {
-	str = colors.yellow(str);
+	    str = colors.yellow(str);
     } else {
-	str = colors.green(str);
+	    str = colors.green(str);
     }
     return str;
 }
+function goodLine (line) {
+    return line
+        && !line.includes("origin/HEAD")
+        && !line.includes("origin/master");
+}
+function makeBranch (line) {
+    var parts = line.replace("origin/", "").split("^~^");
+    this.max  = Math.max(this.max, parts[0].length);
+    return {
+        date:   moment(parts[2]),
+        author: parts[0],
+        name:   parts[1]
+    };
+}
+function sortBranch (a, b) {
+    var order = a.author.localeCompare(b.author);
+    if (order == 0) {
+        order = a.date > b.date;
+    }
+    return order;
+}
+function printBranch (branch) {
+    var gap   = " ".repeat(this.max - branch.author.length);
+    var date  = branch.date;
+    var clean = `[${get_coloured_date(date)}] ${branch.author}${gap} ${branch.name}`;
+    console.log(`${clean}`);
+}
 
 git.stdout.on('data', (data) => {
-
-    var lines    = data.toString().split(/\r?\n/);
-    var branches = [];
-    var max      = 0;
-    lines.forEach((line) => {
-	if (!line || line.includes("origin/HEAD") || line.includes("origin/master")) {
-	    return;
-	}
-	var parts = line.replace("origin/", "").split("^~^");
-	branches.push({
-	    date:   moment(parts[2]),
-	    author: parts[0],
-	    name:   parts[1]
-	});
-	max = Math.max(max, parts[0].length);
-    });
-    branches.sort((a, b) => {
-	var order = a.author.localeCompare(b.author);
-	if (order == 0) {
-	    order = a.date > b.date;
-	}
-	return order;
-    });
-    branches.forEach((branch) => {
-	var gap   = " ".repeat(max - branch.author.length);
-	var date  = branch.date;
-	var clean = `[${get_coloured_date(date)}] ${branch.author}${gap} ${branch.name}`;
-	console.log(`${clean}`);
-    });
+    this.max = 0;
+    data.toString()
+        .split(/\r?\n/)
+        .filter(goodLine)
+        .map(makeBranch, this)
+        .sort(sortBranch)
+        .forEach(printBranch, this);
 });
 
-git.stderr.on('data', (data) => {
+git.stderr.on('data', () => {
     console.log(`Unable to display branches`);
     process.exit(0);
 });
